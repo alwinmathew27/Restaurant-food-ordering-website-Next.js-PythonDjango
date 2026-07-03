2c1c9275745d50c06a19df89eab282abeb888363
Alwin Mathew <alwinmathew27@gmail.com>
2026-07-03 17:30:36 +0530

    Add food rating

--- backend/home/models.py
+++ backend/home/models.py
@@ -4,8 +4,8 @@ class Food(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
     price = models.DecimalField(max_digits=8, decimal_places=2)
+    rating = models.DecimalField(max_digits=2, decimal_places=1, default=0)
     image = models.ImageField(upload_to='foods/')
-
     def __str__(self):
         return self.name
     
--- backend/home/serializers.py
+++ backend/home/serializers.py
@@ -2,6 +2,8 @@ from rest_framework import serializers
 from .models import Food
 
 class FoodSerializer(serializers.ModelSerializer):
+    image = serializers.ImageField(use_url=True)
+
     class Meta:
         model = Food
         fields = "__all__"
